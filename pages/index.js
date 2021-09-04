d8f8e5d8e2ab38fb7f46d045c2565a99e0aedf89
e-roy <ericroupe@gmail.com>
2021-09-04 10:17:54 -0400

    testing

--- pages/index.js
+++ pages/index.js
@@ -43,15 +43,30 @@ export default function Home() {
 
         <p className={styles.description}>Built with NextJS & GPT-3 API</p>
 
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h3>Enter Artist:</h3>
-            <input
-              type="text"
-              value={query}
-              onChange={(event) => setQuery(event.target.value)}
-            />
-            <button type="button" onClick={() => setSearch(query)}>
+        <div className="border p-4 my-8 rounded">
+          <div className="">
+            <label
+              htmlFor="artist"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Enter Artist:
+            </label>
+            <div className="mt-1">
+              <input
+                type="text"
+                name="artist"
+                id="artist"
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm p-2 border border-gray-300 rounded-md"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
+              />
+            </div>
+
+            <button
+              className="px-4 py-2 my-4 bg-blue-400 hover:bg-blue-500 rounded text-white"
+              type="button"
+              onClick={() => setSearch(query)}
+            >
               Generate
             </button>
 
--- util/gpt3Request.js
+++
@@ -1,6 +0,0 @@
-const gpt3Request = () => {
-  console.log("gpt3 request");
-  console.log(process.env.NEXT_PUBLIC_API);
-};
-
-export default gpt3Request;
